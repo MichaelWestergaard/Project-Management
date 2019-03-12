5ac8c83ed9f2c6ff5f0a838dab0a09721816be73
Michael Westergaard <michaelwestergaard@hotmail.dk>
2019-03-12 20:09:12 +0100

    MySQL Connector

---
+++ project_management/MySQLConnector.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_management
+{
+    class MySQLConnector
+    {
+        private MySqlConnection connection { get; set; }
+        private MySQLConnector connector { get; set; }
+
+        private MySQLConnector()
+        {
+            connection = new MySqlConnection("SERVER=mysql25.unoeuro.com;DATABASE=michaelwestergaard_dk_db;UID=michaelwest_dk;PASSWORD=68wukovuzovi;");
+        }
+
+        public MySQLConnector Instance
+        {
+            get
+            {
+                if (connector == null || connector.connection == null)
+                    connector = new MySQLConnector();
+                return connector;
+            }
+        }
+    }
+}
