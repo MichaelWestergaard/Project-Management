dc56fc51bf1af7a6dd7842354ed802fde70d4b26
Alen Hasanagic <Alenhasana@yahoo.dk>
2019-03-21 20:45:54 +0100

    dd

---
+++ project_management/DAO/BaseDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_management.DAO
+{
+    public interface BaseDAO<T>
+    {
+        bool create(T obj);
+        T read(int ID);
+        bool update(T obj);
+        List<T> list();
+        T delete(int ID);
+ 
+
+    }
+}
---
+++ project_management/DAO/UserDAO.cs
@@ -0,0 +1,57 @@
+﻿using project_management.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project_management.Connector;
+
+namespace project_management.DAO
+{
+    class UserDAO : BaseDAO<User>
+    {
+
+        public bool create(User obj)
+        {
+            MySQLConnector mySQLConnector = MySQLConnector.Instance;
+
+            var parameters = new Dictionary<string, string>();
+
+            parameters.Add("@email", "michaelwestergaard@hotmail.dk");
+
+            MySqlDataReader dataReader = mySQLConnector.GetData("SELECT * FROM users WHERE email = @email", parameters);
+
+            while (dataReader.Read())
+            {
+                Console.WriteLine("Username: " + dataReader.GetString("firstname"));
+            }
+
+            mySQLConnector.CloseConnection();
+
+
+
+
+            return true;
+        }
+
+        public User delete(int ID)
+        {
+            throw new NotImplementedException("Denne metode er ikke lavet");
+        }
+
+        public List<User> list()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User read(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(User obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
