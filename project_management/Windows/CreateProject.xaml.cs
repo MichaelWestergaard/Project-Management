3a933a7694122b7dcd62fab457c3187456f90061
Michael Westergaard <michaelwestergaard@hotmail.dk>
2019-03-23 19:27:56 +0100

    Opsat design til oprettelse af projekt

--- project_management/Windows/CreateProject.xaml.cs
+++ project_management/Windows/CreateProject.xaml.cs
@@ -1,5 +1,4 @@
-﻿using project_management.Windows;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +10,27 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace project_management
+namespace project_management.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateProject.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateProject : Window
     {
-        public MainWindow()
+        public CreateProject()
         {
             InitializeComponent();
-            //var home = new Home();
+        }
 
-            //home.Show();
-            new CreateProject().Show();
+        private void Toolbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
     }
