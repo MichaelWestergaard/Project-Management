b1d006034d4b9cc2a4136acb3beabb25b95f4380
Alen Hasanagic <Alenhasana@yahoo.dk>
2019-04-20 18:12:30 +0200

    Oversigt done

--- project_management/ViewModels/OverviewModel.cs
+++ project_management/ViewModels/OverviewModel.cs
@@ -14,7 +14,7 @@ namespace project_management.ViewModels
 
     public class OverviewModel : INotifyPropertyChanged
     {
-        private PlotModel piechart, burndown;
+        private PlotModel piechart, burndown, burnup;
         public PlotModel Piechart
         {
             get { return piechart; }
@@ -27,10 +27,19 @@ namespace project_management.ViewModels
             set { burndown = value; OnPropertyChanged("Burndown"); }
         }
 
+        public PlotModel Burnup
+        {
+            get { return burnup; }
+            set { burnup = value; OnPropertyChanged("Burnup"); }
+        }
+
+
+
         public OverviewModel()
         {
             Piechart = new PlotModel();
             Burndown = new PlotModel();
+            Burnup = new PlotModel();
             SetUpModels();
         }
 
@@ -38,6 +47,7 @@ namespace project_management.ViewModels
         {
             SetUpPiechart();
             SetUpBurndown();
+            SetUpBurnup();
         }
 
         private void SetUpPiechart()
@@ -96,6 +106,65 @@ namespace project_management.ViewModels
 
 
         }
+
+        private void SetUpBurnup()
+        {
+            //Setup the axis
+            var startDate = DateTime.Now.AddDays(-10).Date;
+            var endDate = DateTime.Now.Date;
+
+            double minValue = DateTimeAxis.ToDouble(startDate);
+            double maxValue = DateTimeAxis.ToDouble(endDate);
+
+            var xAxis = new DateTimeAxis { Position = AxisPosition.Bottom, StringFormat = "dd/MM/yy", Title = "Datoer", Minimum = minValue, Maximum = maxValue, IntervalLength = 35, IsPanEnabled = false, IsZoomEnabled = false, MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot };
+            Burnup.Axes.Add(xAxis);
+
+            var yAxis = new LinearAxis { Position = AxisPosition.Left, Title = "Opgaver", Minimum = 0, Maximum = 4, IsPanEnabled = false, IsZoomEnabled = false, MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot };
+            Burnup.Axes.Add(yAxis);
+
+
+            //Setup the actual data
+            var burnupSeries = new LineSeries { StrokeThickness = 2, CanTrackerInterpolatePoints = false };
+
+            burnupSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(-10).Date), 4));
+            burnupSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(-8).Date), 3));
+            burnupSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(-4).Date), 2));
+            burnupSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(-2).Date), 1));
+            burnupSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.Date), 0));
+
+            Burnup.Series.Add(burnupSeries);
+
+
+            //Setup the required pace
+            var burnupLinear = new LineSeries { StrokeThickness = 1, CanTrackerInterpolatePoints = false,
+                MarkerSize = 3,
+                Color = OxyColors.Black,
+                Smooth = true,        
+            };
+
+            int timespan = (endDate - startDate).Days;
+            double timePerDay = 4.0 / timespan;
+
+            Console.WriteLine("Timespan: " + timespan);
+            Console.WriteLine("Time Per Day: " + timePerDay);
+
+            for (int i = 0; i <= timespan; i++)
+            {
+                burnupLinear.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays((0 - i)).Date), i * timePerDay));
+            }
+
+            Burnup.Series.Add(burnupLinear);
+
+
+        }
+
+
+
+
+
+
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
