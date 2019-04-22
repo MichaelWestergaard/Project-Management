4f9d0ef6a79d824993acc9973cb461ee5977e4e8
Alen Hasanagic <Alenhasana@yahoo.dk>
2019-04-22 18:58:23 +0200

    Workload class oprettet

---
+++ project_management/Windows/WorkLog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_management.Windows
+{
+    /// <summary>
+    /// Interaction logic for WorkLog.xaml
+    /// </summary>
+    public partial class WorkLog : Window
+    {
+        public WorkLog()
+        {
+            InitializeComponent();
+        }
+    }
+}
